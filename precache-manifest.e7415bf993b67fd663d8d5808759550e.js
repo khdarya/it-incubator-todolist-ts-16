e0f9bc0586171dedd2eca1cee4e5157396b648b8
khdarya <darya.khakhlova@gmail.com>
2021-01-09 09:38:51 -0500

    Updates

---
+++ precache-manifest.e7415bf993b67fd663d8d5808759550e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b0e10f1fe3b71e84dcc94ab6058430",
+    "url": "/it-incubator-todolist-ts-16/index.html"
+  },
+  {
+    "revision": "21f8ae3323a5152eb0c7",
+    "url": "/it-incubator-todolist-ts-16/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "1872f3d4efb984747105",
+    "url": "/it-incubator-todolist-ts-16/static/js/2.210a29f0.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/it-incubator-todolist-ts-16/static/js/2.210a29f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f8ae3323a5152eb0c7",
+    "url": "/it-incubator-todolist-ts-16/static/js/main.19e92b67.chunk.js"
+  },
+  {
+    "revision": "796eab68e96e5a56fbcb",
+    "url": "/it-incubator-todolist-ts-16/static/js/runtime-main.ed3e583a.js"
+  }
+]);
